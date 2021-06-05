9e682f232b1f1213457a8fb4e39f88c641804355
kkwsb <76960301+kkwsb@users.noreply.github.com>
2021-06-05 16:33:15 -0500

    How'd that get in there?

--- src copy/components/index-section-components.jsx
+++
@@ -1,9 +0,0 @@
-import SectionCallToAction from "./section-cta.jsx";
-import SectionHero from "./section-hero.jsx";
-
-const sectionComponentTypeList = {
-  SectionCallToAction,
-  SectionHero,
-};
-
-export default sectionComponentTypeList;
