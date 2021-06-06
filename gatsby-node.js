9e81afa039238897c6a51c23a7301776640bec2c
kkwsb <76960301+kkwsb@users.noreply.github.com>
2021-06-06 08:34:10 -0500

    blog-post to Post, better export.json handmade

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,19 +18,17 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `);
   const flexipage_nodes = queryResult.data.flexipage.nodes.map((node) => {return {template:'flexible-page', ...node}; });
-  const blogpost_nodes = queryResult.data.blogpost.nodes.map((node) => {return {template:'blog-post', ...node}; });
+  const blogpost_nodes = queryResult.data.blogpost.nodes.map((node) => {return {template:'post', ...node}; });
   const the_nodes = [...flexipage_nodes, ...blogpost_nodes];
   if (!!the_nodes) {
     the_nodes.map(({ slug, template }) => {
-	  //const the_path = (slug === 'index') ? `/` : `/${slug}`;
 	  let the_path = `/`;
-	  if (template === 'blog-post') {
+	  if (template === 'post') {
 		  the_path = the_path + `blog/${slug}`;
 	  } else if (slug !== 'index') {
 		the_path = the_path + slug;
 	  }
       createPage({
-		// (slug) => {if (template === 'blog-post') { return `/blog/${slug}` } else if (slug === 'index') { return `/` } else { return `/${slug}`} }
         path: the_path,
         component: path.resolve(`./src/templates/${template}.jsx`),
         context: { slug },
